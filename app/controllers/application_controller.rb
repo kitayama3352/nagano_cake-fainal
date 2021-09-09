3e9f93eab53e821070b41789ed8d2abd7b0cec0d
EC2 Default User <ec2-user@ip-172-31-12-222.ec2.internal>
2021-09-09 02:10:24 +0000

    終わり

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,6 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
       admins_homes_top_path
-    else
-      root_path
     end
   end
 
